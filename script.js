3cd923ca1bd3df75c02a911c66422a2ab2c613de
Luis232003 <116083626+Luis232003@users.noreply.github.com>
2023-03-21 22:11:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const openEnvelopeButton = document.getElementById('open-envelope');
 const closeEnvelopeButton = document.getElementById('close-envelope');
 const envelope = document.querySelector('.envelope');
+
 openEnvelopeButton.addEventListener('click', function() {
-	envelope.style.display = 'block';
+  envelope.style.display = 'block';
 });
 
 closeEnvelopeButton.addEventListener('click', function() {
-	envelope.style.display = 'none';
+  envelope.style.display = 'none';
 });
